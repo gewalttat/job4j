c8f9fa9517d61cc7ea13b1944fa6b2ccd194004b
gewalttat <mailto:rbhorses@mail.ru>
2018-07-28 13:35:49 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,24 @@
-package ru.job4j.calculate;
+package ru.parsentev;
+/**
+* Calculate.
+*
+* @author Trishin Ivan (rbhorses@mail.ru)
+*/
 public class Calculate {
-public static void main (String[] args) {
-System.out.println("Hello World");}}
+/**
+* Method echo.
+* @param value
+* @return "%s"
+*/
+public String echo(String value) {
+return String.format("%s %s %s", value, value, value);
+}
+/**
+* Main.
+* @param args - args
+*/
+public static void main(String[] args){
+Calculate calc = new Calculate();
+System.out.println(calc.echo("aah"));
+}
+}
