85da8e3ea95242e6b92c1af5db608c4b133dc93e
gewalttat <mailto:rbhorses@mail.ru>
2018-07-31 22:05:50 +0300

    123

---
+++ chapter_001/src/main/java/ru/job4j/calculate/HW.java
@@ -0,0 +1,7 @@
+package ru.job4j.calculate;
+
+public class HW {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculate task.
-*
-* @author Ivan Trishin (mailto:rbhorses@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j.calculate;
