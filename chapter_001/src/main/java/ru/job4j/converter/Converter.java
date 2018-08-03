27802388ddd742219e8dc982417ec6ac3a58d355
gewalttat <mailto:rbhorses@mail.ru>
2018-08-03 22:09:34 +0300

    *3.2. Конвертер валюты*

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,11 +1,13 @@
 package ru.job4j.converter;
-/** 
+
+/**
  * Корвертор валюты.
  */
 public class Converter {
 
     /**
      * Конвертируем рубли в евро.
+     *
      * @param value рубли.
      * @return Евро.
      */
@@ -15,10 +17,31 @@ public class Converter {
 
     /**
      * Конвертируем рубли в доллары.
+     *
      * @param value рубли.
      * @return Доллары
      */
     public int rubleToDollar(int value) {
-        return value / 70;
+        return value / 60;
+    }
+
+    /**
+     * Конвертируем доллары в рубли.
+     *
+     * @param value доллары.
+     * @return рубли
+     */
+    public int DollarToRuble(int value) {
+        return value / 1;
+    }
+
+    /**
+     * Конвертируем евро в рубли.
+     *
+     * @param value евро.
+     * @return рубли.
+     */
+    public int EuroToRuble(int value) {
+        return value / 1;
     }
 }
--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -9,7 +9,7 @@ public class ConverterTest {
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
         int result = converter.rubleToDollar(60);
-        assertThat(result, is(0));
+        assertThat(result, is(1));
     }
 
     @Test
@@ -17,5 +17,17 @@ public class ConverterTest {
         Converter converter = new Converter();
         int result = converter.rubleToEuro(70);
         assertThat(result, is(1));
+    }
+    @Test
+    public void whenDollarTo60RubThen1() {
+        Converter converter = new Converter();
+        int result = converter.DollarToRuble(60);
+        assertThat(result, is(60));
+    }
+    @Test
+    public void whenEuroTo70RubThen1() {
+        Converter converter = new Converter();
+        int result = converter.EuroToRuble(70);
+        assertThat(result, is(70));
     }
 }
