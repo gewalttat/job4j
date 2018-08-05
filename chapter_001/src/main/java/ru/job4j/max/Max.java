0bfdf96ad60cfc6da41dbf3a743fd7c65f376b31
gewalttat <mailto:rbhorses@mail.ru>
2018-08-05 17:58:14 +0300

    4.2 максимум из двух чисел чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,9 +2,6 @@ package ru.job4j.max;
 
 public class Max {
     public int max(int first, int second) {
-        if (first > second) {
-            return first;
-        } else if (second > first) ;
-        return second;
+return first>second ? first : second;
     }
 }
