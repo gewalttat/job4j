467f50d3500dc5c746848107236a524b4f043754
gewalttat <mailto:rbhorses@mail.ru>
2018-08-22 21:39:21 +0300

    задача к контрольным вопросам

--- chapter_001/src/test/java/ru/job4j/array/Quertytest.java
+++ chapter_001/src/test/java/ru/job4j/array/Quertytest.java
@@ -3,16 +3,14 @@ import org.junit.Test;
 /*
 задача на сортировку массива к контрольным вопросам по 1 блоку
  */
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-public class quertytest {
+public class Quertytest {
     @Test
     public void arraySortWithSixElements() {
-        BubbleSort bs = new BubbleSort();
-        int[] array = new int[]{5, 1, 2, 7, 3, 9};
-        int[] result = bs.sort(array);
-        int[] expect = new int[]{1, 2, 3, 5, 7, 9};
-        assertThat(result, is(expect));
+        Qwerty qw = new Qwerty();
+        int input[]  = new int[]{7, 1, 9, 4, 2};
+        boolean result = qw.isSort(input);
+        assertThat(result, is(true));
     }
 }
