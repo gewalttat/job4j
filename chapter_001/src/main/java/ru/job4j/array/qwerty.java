27d3264064cc22fd223c45b1a9630dfe1dd39da8
gewalttat <mailto:rbhorses@mail.ru>
2018-08-23 21:48:56 +0300

    задача на проверку сортировки

--- chapter_001/src/main/java/ru/job4j/array/qwerty.java
+++ chapter_001/src/main/java/ru/job4j/array/qwerty.java
@@ -1,17 +1,13 @@
 package ru.job4j.array;
 
-public class Qwerty {
+public class qwerty {
     public boolean isSort(int[] a) {
         boolean result;
         for (int i = 0; i < a.length - 1; i++) {
             if (a[i] > a[i + 1]) {
                 result = false;
-            } else {
-                for (int j = 0; j < a.length - 1; j++) {
-                    if (a[i] < a[j + 1]) {
-                        result = false;
-                    }
-                }
+            } else if (a[i] < a[i + 1]) {
+                result = false;
             }
         }
         return true;
