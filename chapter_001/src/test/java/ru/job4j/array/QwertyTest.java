1515be715706c6b46a56738c0bf04797c3c60dc3
gewalttat <mailto:rbhorses@mail.ru>
2018-08-25 23:39:38 +0300

    задача на проверку сортировки

--- chapter_001/src/test/java/ru/job4j/array/QwertyTest.java
+++ chapter_001/src/test/java/ru/job4j/array/QwertyTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.array;
+
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-public class Quertytest {
+
+public class QwertyTest {
     @Test
     public void arraySortWithSixElements() {
         Qwerty qw = new Qwerty();
-        int input[]  = new int[]{7, 1, 9, 4, 2};
+        int input[] = new int[]{1, 2, 4, 7, 9};
         boolean result = qw.isSort(input);
         assertThat(result, is(true));
     }
