86706baa92c94b179d8fef6b4a85c84725c35d72
Ivan Trishin <rbhorses@mail.ru>
2018-09-17 15:42:41 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.util.ArrayList;
 import java.util.List;
 public class StartUI {
-
+    private int ranges[] = new int[]{1, 2, 3, 4};
 
     private final Input input;
     private final Tracker tracker;
@@ -22,11 +22,10 @@ public class StartUI {
         }
         do {
             menu.show();
-            menu.select(Integer.valueOf(input.ask("Select option:")));
+            menu.select(input.ask("Select option:", ranges));
         } while (!"y".equals(this.input.ask("Exit?(y): ")));
     }
-
     public static void main(String[] args) {
-        new StartUI(new ConsoleInput(), new Tracker()).init();
+        new StartUI(new ValidateInput(), new Tracker()).init();
     }
 }
---
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class ValidateInput extends ConsoleInput {
+    public int ask(String question, int[] range) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.ask(question, range);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data: ");
+            } catch (MenuOutException moe) {
+                System.out.println("Please select key from menu");
+            }
+        }
+        while (invalid);
+        return value;
+    }
+}
