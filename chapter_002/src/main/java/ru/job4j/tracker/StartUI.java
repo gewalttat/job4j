0513775e2b9fb940a06f3cb6975fe98068719d58
Ivan Trishin <rbhorses@mail.ru>
2018-09-17 16:15:33 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.util.ArrayList;
 import java.util.List;
 public class StartUI {
-    private int ranges[] = new int[]{1, 2, 3, 4};
+    private int[] ranges = new int[]{1, 2, 3, 4};
 
     private final Input input;
     private final Tracker tracker;
@@ -25,7 +25,13 @@ public class StartUI {
             menu.select(input.ask("Select option:", ranges));
         } while (!"y".equals(this.input.ask("Exit?(y): ")));
     }
+
     public static void main(String[] args) {
-        new StartUI(new ValidateInput(), new Tracker()).init();
+        new StartUI(
+                new ValidateInput(
+                        new ConsoleInput()
+                ),
+                new Tracker()
+        ).init();
     }
 }
