468b6e4c8b0e2c38ee437580eb41b6bba9dda913
Ivan Trishin <rbhorses@mail.ru>
2018-09-19 13:07:14 +0300

    3. if-else-throw

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -12,20 +12,12 @@ public class ValidateInput implements Input {
     public String ask(String question) {
         return this.input.ask(question);
     }
+
     public int ask(String question, List<Integer> range) {
-        boolean invalid = true;
-        int value = -1;
-        do {
-            try {
-                value = this.input.ask(question, range);
-                invalid = false;
-            } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data:");
-            } catch (MenuOutException moe) {
-                System.out.println("Please select key from menu");
-            }
+        int key = this.input.ask(question, range);
+        if (!range.contains(key)) {
+            throw new MenuOutException("Please enter validate data:");
         }
-        while (invalid);
-        return value;
+        return key;
     }
 }
